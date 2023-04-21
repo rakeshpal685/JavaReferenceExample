87cba22bce99b4bd9d0f82ad4750bae550de0b65
rakeshpal685 <rakesh.pal685@gmail.com>
2023-04-22 02:05:25 +0800

    Comparator example

--- src/main/java/com/JavaReference/example/comparableVscomparator/Employee.java
+++ src/main/java/com/JavaReference/example/comparableVscomparator/Employee.java
@@ -5,6 +5,8 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
+//Here I am using Comparable and overriding the compareTo method to provide sorting based on decreasing id
+//number
 public class Employee implements Comparable<Employee> {
 
   private int id;
--- src/main/java/com/JavaReference/example/comparableVscomparator/Testing.java
+++ src/main/java/com/JavaReference/example/comparableVscomparator/Testing.java
@@ -1,18 +1,35 @@
 package com.JavaReference.example.comparableVscomparator;
 
-import org.springframework.web.servlet.tags.form.SelectTag;
-
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Testing {
   public static void main(String[] args) {
 
-    Set<Employee> employeeSet = new HashSet<>();
-    employeeSet.add(new Employee(1, "rakesh"));
-    employeeSet.add(new Employee(3, "mukesh"));
-    employeeSet.add(new Employee(5, "ali"));
-    employeeSet.add(new Employee(2, "john"));
-    employeeSet.add(new Employee(4, "abc"));
+    List<Employee> employeeList = new LinkedList<>();
+    employeeList.add(new Employee(1, "rakesh"));
+    employeeList.add(new Employee(3, "mukesh"));
+    employeeList.add(new Employee(5, "ali"));
+    employeeList.add(new Employee(2, "john"));
+    employeeList.add(new Employee(4, "abc"));
+
+    // Here if I sort my employee object, it will sort by id, as specified in the Employee class.
+    Collections.sort(employeeList);
+
+    /* Here I am dynamically providing the sorting logic using comparator,
+     as sort can take comparator as argument and comparator is functional interface, I can directly give implementation
+    by lambda*/
+    employeeList.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
+
+    // System.out.println(employeeList);
+
+    // I can use comparator as an argument to TreeSet because TreeSet sort the elements
+    Set<Employee> employeeSet = new TreeSet<>((o1, o2) -> o1.getName().compareTo(o2.getName()));
+    employeeSet.add(new Employee(1, "abc"));
+    employeeSet.add(new Employee(3, "aaa"));
+    employeeSet.add(new Employee(2, "bbb"));
+
+    System.out.println(employeeSet);
   }
+
+  // If wwe use both comparable and comparator together then comparator will have more precedence.
 }
